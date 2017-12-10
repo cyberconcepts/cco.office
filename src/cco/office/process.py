702c492dc8276da92bb6e7cbc720f4c926a3a3c2
Helmut Merz <helmutm@cy55.de>
2017-12-10 18:23:48 +0100

    start LibreOffice if necessary

--- src/cco/office/process.py
+++ src/cco/office/process.py
@@ -8,17 +8,36 @@
 Process data using a LibreOffice file.
 """
 
+import os
+from subprocess import run, Popen, STDOUT, PIPE
+import sys
+import time
+
 import pyoo
 
 
+command = ['soffice', '--accept="socket,host=localhost,port=2002;urp;"',
+           '--norestore', '--nologo', '--nodefault', '--headless', '&']
 command = ('soffice --accept="socket,host=localhost,port=2002;urp;" '
-           '--norestore --nologo --nodefault --headless')
+           '--pidfile=var/pid/office.pid '
+           '--norestore --nologo --nodefault --headless &')
 
 def run():
+    desktop = None
     try:
         desktop = pyoo.Desktop('localhost', 2002)
     except OSError:
-        print('*** Error: Office connection could not be established.')
+        print('*** Info: Starting office application.')
+        with Popen(command, stdout=PIPE, stderr=PIPE, shell=True) as proc:
+            #print(proc.stdout.read())
+            #print(proc.stderr.read())
+            print('*** Info: Office application started:', proc)
+            time.sleep(5)
+        try:
+            desktop = pyoo.Desktop('localhost', 2002)
+        except OSError:
+            print('*** Error: Office connection could not be established.')
+    print('***', desktop)
 
 
 if __name__ == '__main__':
