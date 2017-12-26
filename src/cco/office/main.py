4456e66841cae1d1b26c62c078c12d9a01660e7a
Helmut Merz <helmutm@cy55.de>
2017-12-26 12:14:47 +0100

    provide module with pyoo extensions

--- src/cco/office/main.py
+++ src/cco/office/main.py
@@ -12,7 +12,7 @@ from logging import getLogger
 import sys
 import pyoo
 
-from cco.office import launch
+from cco.office import launch, pyoox
 
 
 logger = getLogger('cco.office')
@@ -28,6 +28,9 @@ def run(dataFn='reportdata.csv', resultFn='report.ods', tplFn='template.ods'):
 def process(desktop, data, resultPath, tplPath):
     doc = desktop.open_spreadsheet(tplPath)
     sheet = doc.sheets[0]
+    dayCells = pyoox.getCellsByName(sheet, 'day')
+    print('***', dayCells)
+    doc.sheets.copy(sheet.name, sheet.name + '-copy', 0)
     doc.save(resultPath) #, pyoo.FILTER_EXCEL_2007)
     doc.close()
     return 0
---
+++ src/cco/office/pyoox.py
@@ -0,0 +1,17 @@
+#
+#  Copyright (C) 2017 cyberconcepts.org team
+#
+#  See the file LICENSE for license details
+#
+
+"""
+Some useful extension to the pyoo library.
+"""
+
+import pyoo
+
+
+def getCellsByName(sheet, name):
+    rawCells = sheet._target.getCellRangeByName(name)
+    addr = rawCells.getRangeAddress()
+    return pyoo.TabularCellRange(sheet, pyoo.SheetAddress._from_uno(addr))
